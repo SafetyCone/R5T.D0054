bd68d628d1619ae41570c588e3bebf62458d65a4
David Coats <david@getsafetycone.com>
2021-04-30 15:17:31 -0700

    Add R5T.D0054.Base service definitions library. Remove extraneous R5T.D0054 library. Add R5T.T0014.Abstractions (ISendAsInformation abstraction) project-reference dependency.

---
+++ source/R5T.D0054.Base/Code/Services/Definitions/IGmailSendAsInformationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0014;
+
+
+namespace R5T.D0054
+{
+    public interface IGmailSendAsInformationProvider
+    {
+        Task<ISendAsInformation> GetGmailSendAsInformation();
+    }
+}
